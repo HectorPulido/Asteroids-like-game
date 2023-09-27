6573cf45506d3ea915ee9b5fbba14511d56c2968
Mario López <mario.ld1@hotmail.com>
2023-09-28 00:24:36 +0200

    Controles forward

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     private Rigidbody2D rb;
     public float acceleration;
     public float maxSpeed;
@@ -17,55 +18,65 @@ public class Player : MonoBehaviour {
     private bool shooting;
     private bool canShoot = true;
 
-    private void Start () {
-        rb = GetComponent<Rigidbody2D> ();
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
         rb.drag = inertia;
     }
 
-    private void Update () {
+    private void Update()
+    {
         vertical = InputManager.Vertical;
         horizontal = InputManager.Horizontal;
         shooting = InputManager.Fire;
 
-        Rotate ();
-        Shoot ();
+        Rotate();
+        Shoot();
     }
 
-    private void Shoot () {
-        if (shooting && canShoot) {
-            StartCoroutine (FireRate ());
+    private void Shoot()
+    {
+        if (shooting && canShoot)
+        {
+            StartCoroutine(FireRate());
         }
     }
 
-    private void Rotate () {
-        if (horizontal == 0) {
+    private void Rotate()
+    {
+        if (horizontal == 0)
+        {
             return;
         }
-        transform.Rotate (0, 0, -angularSpeed * horizontal * Time.deltaTime);
+        transform.Rotate(0, 0, -angularSpeed * horizontal * Time.deltaTime);
     }
 
-    private void FixedUpdate () {
-        var forwardMotor = Mathf.Clamp (vertical, 0f, 1f);
-        rb.AddForce (transform.up * acceleration * forwardMotor);
-        if (rb.velocity.magnitude > maxSpeed) {
+    private void FixedUpdate()
+    {
+        var forwardMotor = Mathf.Clamp(vertical, 0f, 1f);
+        rb.AddForce(transform.up * acceleration * forwardMotor);
+        if (rb.velocity.magnitude > maxSpeed)
+        {
             rb.velocity = rb.velocity.normalized * maxSpeed;
         }
     }
 
-    public void Lose () {
+    public void Lose()
+    {
         rb.velocity = Vector3.zero;
         transform.position = Vector3.zero;
     }
 
-    private IEnumerator FireRate () {
+    private IEnumerator FireRate()
+    {
         canShoot = false;
-        var bullet = Instantiate (
+        var bullet = Instantiate(
             bulletPrefab,
             bulletSpawner.position,
             bulletSpawner.rotation
         );
-        Destroy (bullet, 5);
-        yield return new WaitForSeconds (shootRate);
+        Destroy(bullet, 5);
+        yield return new WaitForSeconds(shootRate);
         canShoot = true;
     }
 }
